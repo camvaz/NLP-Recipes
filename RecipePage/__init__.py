3d3308283383eafa090662cd7f9e8034d6b2f3c6
Victor Campos <camposvazquezv@gmail.com>
2021-01-26 01:34:30 -0600

    feat: code formatting with black

--- RecipePage/__init__.py
+++ RecipePage/__init__.py
@@ -6,42 +6,53 @@ from typing import List
 from nltk.corpus import stopwords
 from unicodedata import normalize
 
+
 def cleanNFD(s):
     return re.sub(
-        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
-        normalize("NFD", s), 0, re.I
+        r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+",
+        r"\1",
+        normalize("NFD", s),
+        0,
+        re.I,
     )
+
+
+def extractIngredients(ingredients):
+    spans = [x.find_all("span") for x in ingredients]
+    spanValues = [[cleanNFD(y.string) for y in x] for x in spans]
+    tokens = [[nltk.word_tokenize(y) for y in x] for x in spanValues]
+    filtered_tokens = [
+        [[w for w in x if not w.lower() in stopwords.words("spanish")] for x in y]
+        for y in tokens
+    ]
+    joinedValues = [[" ".join(y) for y in x] for x in filtered_tokens]
+    joinedSequences = [" ".join(x) for x in joinedValues]
+    return joinedSequences
 
 
 def extract(URL):
     page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    results = soup.find(id='main')
+    results = soup.find(id="main")
 
-    ingredientElements = results.find_all('li', class_='simmer-ingredient')
-    instructionElements = results.find_all('li', class_='simmer-instruction')
-
-    def extractIngredients(ingredients):
-        spans = [x.find_all('span') for x in ingredients]
-        spanValues = [[cleanNFD(y.string) for y in x] for x in spans]
-        tokens = [[nltk.word_tokenize(y) for y in x] for x in spanValues]
-        filtered_tokens = [[[w for w in x if not w.lower() in stopwords.words('spanish')] for x in y] for y in tokens]
-        joinedValues = [[" ".join(y) for y in x] for x in filtered_tokens]
-        joinedSequences = [" ".join(x) for x in joinedValues]
-        return joinedSequences
+    ingredientElements = results.find_all("li", class_="simmer-ingredient")
+    instructionElements = results.find_all("li", class_="simmer-instruction")
 
     ingredientList = extractIngredients(ingredientElements)
     cleanInstructions = [cleanNFD(x.string) for x in instructionElements]
     instructionTokens = [nltk.word_tokenize(x) for x in cleanInstructions]
-    sanitizedInstructions = [[w for w in x if not w.lower() in stopwords.words('spanish')] for x in instructionTokens] 
+    sanitizedInstructions = [
+        [w for w in x if not w.lower() in stopwords.words("spanish")]
+        for x in instructionTokens
+    ]
     joinedInstructions = [" ".join(x) for x in sanitizedInstructions]
 
     result = {
         "title": cleanNFD(soup.title.string),
         "ingredients": ingredientList,
-        "instructions": joinedInstructions
+        "instructions": joinedInstructions,
     }
 
     return result
--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import json
 from RecipePage import extract
 from bs4 import BeautifulSoup
 from typing import List
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer  
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-URL = 'https://www.cocinafacil.com.mx/recetas/recetas-para-hot-cakes/'
+URL = "https://www.cocinafacil.com.mx/recetas/recetas-para-hot-cakes/"
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id='post-27696')
+results = soup.find(id="post-27696")
 
-liElements = results.find_all('li', class_='simmer-connected-recipe')
+liElements = results.find_all("li", class_="simmer-connected-recipe")
 
-anchors = [x.find_all('a') for x in liElements]
-hrefs = [x[0].get('href') for x in anchors]
+anchors = [x.find_all("a") for x in liElements]
+hrefs = [x[0].get("href") for x in anchors]
 
 hrefList: List[str] = hrefs
 
-print('Recetas: ')
+print("Recetas: ")
 print(hrefList)
 
 recipePages = map(extract, hrefList)
 res = list(recipePages)
-corpus = [" ".join(x['ingredients']) + " ".join(x['instructions']) for x in res]
+corpus = [" ".join(x["ingredients"]) + " ".join(x["instructions"]) for x in res]
 namesFromCorpus = [x.split(" ") for x in corpus]
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(corpus)
@@ -32,35 +32,39 @@ names = vectorizer.get_feature_names()
 farray = X.toarray().tolist()
 transformer = TfidfTransformer(smooth_idf=False)
 tfidf = transformer.fit_transform(farray)
-tfidfArray = tfidf.toarray().tolist() 
+tfidfArray = tfidf.toarray().tolist()
 
 collection = []
 for i in range(len(tfidfArray)):
-    collection.append({"recipe": res[i]['title'], "words":[] })
+    collection.append({"recipe": res[i]["title"], "words": []})
     for y in range(len(tfidfArray[i])):
-        collection[i]['words'].append({ "name": names[y], "tf-idf": tfidfArray[i][y] })
-    collection[i]['words'].sort(reverse=True,key=lambda x: x['tf-idf'])
+        collection[i]["words"].append({"name": names[y], "tf-idf": tfidfArray[i][y]})
+    collection[i]["words"].sort(reverse=True, key=lambda x: x["tf-idf"])
 
-tfLazyResults = [{**z, "words":filter(lambda y: y['tf-idf'] != 0, z['words'])} for z in collection]
-tfResults = [{**x, "words":list(x['words'])} for x in tfLazyResults]
+tfLazyResults = [
+    {**z, "words": filter(lambda y: y["tf-idf"] != 0, z["words"])} for z in collection
+]
+tfResults = [{**x, "words": list(x["words"])} for x in tfLazyResults]
 
 wordTotal = {}
 for i in range(len(tfResults)):
-    for j in range(len(tfResults[i]['words'])):
-        currentKey = tfResults[i]['words'][j]['name']
-        if(not currentKey in wordTotal):
-            wordTotal[currentKey] = tfResults[i]['words'][j]['tf-idf']
+    for j in range(len(tfResults[i]["words"])):
+        currentKey = tfResults[i]["words"][j]["name"]
+        if not currentKey in wordTotal:
+            wordTotal[currentKey] = tfResults[i]["words"][j]["tf-idf"]
         else:
-            wordTotal[currentKey] = tfResults[i]['words'][j]['tf-idf'] + wordTotal[currentKey]
+            wordTotal[currentKey] = (
+                tfResults[i]["words"][j]["tf-idf"] + wordTotal[currentKey]
+            )
 
-sortedWordTotal = dict(sorted(wordTotal.items(), key=lambda item: item[1], reverse=True))
+sortedWordTotal = dict(
+    sorted(wordTotal.items(), key=lambda item: item[1], reverse=True)
+)
 
-pagesData = {
-    "data": tfResults
-}
+pagesData = {"data": tfResults}
 
-with open("PagesData.json","w") as outfile:
-    json.dump(tfResults,outfile)
+with open("PagesData.json", "w") as outfile:
+    json.dump(tfResults, outfile)
 
 with open("Data.json", "w") as outfile:
     json.dump(wordTotal, outfile)
